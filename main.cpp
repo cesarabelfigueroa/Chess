20f49fe8505dd84decdc369e2b9e8e383d67dce4
César Figueroa <cesarabelfigueroa@unitec.edu>
2016-02-27 17:38:09 -0600

    show board

--- main.cpp
+++ main.cpp
@@ -1,13 +1,13 @@
 /*---- Standard C++ directives ----*/
 #include <iostream>
-#include <curses.h>
 #include <cstdlib>
 #include <cctype>
 #include <cstring>
-#include <curses.h>
+#include <string>
 
 /*---- NCurses directives ----*/
 #include <menu.h>
+#include <panel.h>
 #include <ncurses.h>
 
 /*---- Chess directives ----*/
@@ -32,35 +32,39 @@ using std::string;
 bool isValiEntry(string move);
 Position* createPosition(char, char);
 int valueOfChar(char);
+Piece*** createBoard();
+void showBoard(Piece***);
 
-int main(int argc, char const *argv[]){
-
-	Piece* pieza = new Peon(NEGRO);
-	cout << pieza-> getType() << endl;
-	Piece* pieza2 = new Alfil(NEGRO);
-	cout << pieza2 -> getType() << endl;
-	Piece* pieza3 = new Caballo(NEGRO);
-	cout << pieza3 -> getType() << endl;
-	Piece* pieza4 = new Torre(NEGRO);
-	cout << pieza4 -> getType() << endl;
-	Piece* pieza5 = new Rey(NEGRO);
-	cout << pieza5 -> getType() << endl;
-	Piece* pieza6 = new Dama(NEGRO);
-	cout << pieza6 -> getType() << endl;
-
-	/*if(isValiEntry(move)){
-		Position* initial = createPosition(move[0], move[1]);
-		Position* final = createPosition(move[2], move[3]);
-		cout << initial -> getX() << endl;
-		cout << initial -> getY() << endl;
-		initial -> ~Position();
-		initial -> ~Position();
-	}else{
-		cout << "Es inválido." << endl;
-	}*/
-
+int main(int argc, char *argv[]){
+ 	Piece*** board = createBoard();
+	initscr();	
+	start_color();
+	init_pair(1, COLOR_BLACK, 0);
+	init_pair(2, COLOR_WHITE, 0);
+	showBoard(board);
+	getch();	
+	endwin();			
 	return 0;
 }
+
+void showBoard(Piece*** board){
+	printw("\n\n");
+	printw("\t A \t B \t C \t D \t E \t F \t G \t H \t\n\n\n");
+	for( int i = 0; i < 8; i++ ){
+		addch(i + 48);
+		printw("\t");
+		for (int j = 0; j < 8; ++j){
+			printw("[");
+			attron( COLOR_PAIR(board[i][j] -> getColor() + 1) | A_BOLD );
+			addch(board[i][j] -> toString());
+			attroff(COLOR_PAIR(board[i][j] -> getColor() + 1));
+			printw("]");
+			printw("\t");
+		}
+		printw("\n\n\n");
+	}
+}
+
 
 Position* createPosition(char x, char y){
 	int file = valueOfChar(x);
@@ -71,26 +75,26 @@ Position* createPosition(char x, char y){
 
 int valueOfChar(char character){
 	if(isalpha(character)){
-		if(character == 'a'){
+		if(tolower(character) == 'a'){
 			return 0;
-		}else if(character == 'b'){
+		}else if(tolower(character) == 'b'){
 			return 1;
-		}else if(character == 'c'){
+		}else if(tolower(character) == 'c'){
 			return 2;
 
-		}else if(character == 'd'){
+		}else if(tolower(character) == 'd'){
 			return 3;
 
-		}else if(character == 'e'){
+		}else if(tolower(character) == 'e'){
 			return 4;
 
-		}else if(character == 'f'){
+		}else if(tolower(character) == 'f'){
 			return 5;
 
-		}else if(character == 'g'){
+		}else if(tolower(character) == 'g'){
 			return 6;
 
-		}else if(character == 'h'){
+		}else if(tolower(character) == 'h'){
 			return 7;
 		}else{
 			return -1;
@@ -110,4 +114,38 @@ bool isValiEntry(string move){
 		}
 	}
 	return true;
+}
+
+Piece*** createBoard(){
+	Piece*** board = new Piece**[8];
+	for (int i = 0; i < 8; ++i){
+		board[i] = new Piece*[8];
+	}
+
+	for (int i = 0; i < 8; ++i){
+		for (int j = 0; j < 8; ++j){
+			ENUM_COLOR color = NULO;
+			if(i == 0 || i == 1){
+				color = NEGRO;
+			}else if(i == 6 || i == 7){
+				color = BLANCO;
+			}
+			if(i == 0 && j == 0 || i == 0 && j == 7 || i == 7 && j == 0 || i == 7 && j == 7){
+				board[i][j] = new Torre(color);
+			}else if(i == 0 && j == 1 || i == 0 && j == 6 || i == 7 && j == 1 || i == 7 && j == 6){
+				board[i][j] =  new Caballo(color);
+			}else if(i == 0 && j == 2 || i == 0 && j == 5 || i == 7 && j == 2 || i == 7 && j == 5){
+				board[i][j] =  new Alfil(color);
+			}else if(i == 0 && j == 3 || i == 7 && j == 3){
+				board[i][j] = new Dama(color);
+			}else if(i == 0 && j == 4 || i == 7 && j == 4){
+				board[i][j] =  new Rey(color);
+			}else if(i == 1 || i == 6){
+				board[i][j] =  new Peon(color);
+			}else{
+				board[i][j] = new Piece(color);
+			}
+		}
+	}
+	return board;
 }
