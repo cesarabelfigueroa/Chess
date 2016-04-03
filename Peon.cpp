0283fcca54896728ddc48aa9d6dc392f0531b8f6
César Figueroa <cesarabelfigueroa@unitec.edu>
2016-04-02 18:45:35 -0600

    auto ident fix

--- Peon.cpp
+++ Peon.cpp
@@ -2,39 +2,39 @@
 #include "Enum.h"
 #include "ncurses.h"
 
-Peon::Peon(ENUM_COLOR color): Piece(color){
-  this-> color = color;
-  Piece::setType(PEON);
+Peon::Peon(ENUM_COLOR color): Piece(color) {
+	this-> color = color;
+	Piece::setType(PEON);
 }
 
-char Peon::toString(){
-  return 'P';
+char Peon::toString() {
+	return 'P';
 }
 
-bool Peon::isValidMove(Position* origen, Position* destino){
+bool Peon::isValidMove(Position* origen, Position* destino) {
 	int initX = origen -> getX();
 	int initY = origen -> getY();
 	int endX = destino -> getX();
 	int endY = destino -> getY();
-	if(initY == endY){
-		if(color == BLANCO){
-			if(initX == 6 && initX <= endX + 2  && endY == initY){
+	if (initY == endY) {
+		if (color == BLANCO) {
+			if (initX == 6 && initX <= endX + 2  && endY == initY) {
 				return true;
-			}else if(initX != 6 && initX == endX + 1 && endY == initY){
+			} else if (initX != 6 && initX == endX + 1 && endY == initY) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
-		}else if(color == NEGRO){
-			if(initX == 1 && endX <= initX + 2 && endY == initY){
+		} else if (color == NEGRO) {
+			if (initX == 1 && endX <= initX + 2 && endY == initY) {
 				return true;
-			}else if(initX != 1 && endX == initX+1 && endY == initY){
+			} else if (initX != 1 && endX == initX + 1 && endY == initY) {
 				return true;
-			}else{
+			} else {
 				return false;
 			}
 		}
-	}else{
+	} else {
 		return false;
 	}
 }
--- Torre.cpp
+++ Torre.cpp
@@ -1,25 +1,25 @@
 #include "Torre.h"
 #include "Enum.h"
 
-Torre::Torre(ENUM_COLOR color): Piece(color){
-  this-> color = color;
-  Piece::setType(TORRE);
+Torre::Torre(ENUM_COLOR color): Piece(color) {
+	this-> color = color;
+	Piece::setType(TORRE);
 }
 
-char Torre::toString(){
-  return 'T';
+char Torre::toString() {
+	return 'T';
 }
 
-bool Torre::isValidMove(Position* origen, Position* destino){
+bool Torre::isValidMove(Position* origen, Position* destino) {
 	int initX = origen -> getX();
 	int initY = origen -> getY();
 	int endX = destino -> getX();
 	int endY = destino -> getY();
-	if(initX == endX && initY != endY){
+	if (initX == endX && initY != endY) {
 		return true;
-	}else if(initY == endY && initX != endX){
+	} else if (initY == endY && initX != endX) {
 		return true;
-	}else{
+	} else {
 		return false;
 	}
 }
